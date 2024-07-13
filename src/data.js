3f11a52072e1df96a429c4945d5ddcae4f9e7bc3
vaibhavugale <ugalevaibhav00@gmail.com>
2024-07-13 13:49:55 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const data = [
+    {
+        questionText: 'What is the capital of France?',
+        options: [
+          { text: 'Berlin', isCorrect: false },
+          { text: 'Madrid', isCorrect: false },
+          { text: 'Paris', isCorrect: true },
+          { text: 'Lisbon', isCorrect: false },
+        ],
+      },
+      {
+        questionText: 'Who is the CEO of Tesla?',
+        options: [
+          { text: 'Jeff Bezos', isCorrect: false },
+          { text: 'Elon Musk', isCorrect: true },
+          { text: 'Bill Gates', isCorrect: false },
+          { text: 'Tony Stark', isCorrect: false },
+        ],
+      },
+      {
+        questionText: 'What is the smallest prime number?',
+        options: [
+          { text: '0', isCorrect: false },
+          { text: '1', isCorrect: false },
+          { text: '2', isCorrect: true },
+          { text: '3', isCorrect: false },
+        ],
+      },
+      {
+        questionText: 'Which planet is known as the Red Planet?',
+        options: [
+          { text: 'Earth', isCorrect: false },
+          { text: 'Mars', isCorrect: true },
+          { text: 'Jupiter', isCorrect: false },
+          { text: 'Saturn', isCorrect: false },
+        ],
+      },
+      {
+        questionText: 'What is the chemical symbol for water?',
+        options: [
+          { text: 'H2O', isCorrect: true },
+          { text: 'O2', isCorrect: false },
+          { text: 'CO2', isCorrect: false },
+          { text: 'NaCl', isCorrect: false },
+        ],
+      },
+    
+]
